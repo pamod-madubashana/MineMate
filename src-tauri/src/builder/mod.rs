63ea559d088764de8d929481293c3bbb774559c6
pamod-madubashana <pamod.main@gmail.com>
2026-07-15 00:21:14 +0530

    chore: export CreativeInventoryManager from builder module

--- src-tauri/src/builder/mod.rs
+++ src-tauri/src/builder/mod.rs
@@ -1,8 +1,10 @@
 #![allow(dead_code)]
 
+pub mod creative_manager;
 pub mod executor;
 pub mod planner;
 pub mod placement;
 pub mod scaffolding;
 
 pub use executor::BuildExecutor;
+pub use creative_manager::CreativeInventoryManager;
